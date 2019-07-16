4fba9a0e34945fdb47378303ee1976453f976ba7
MeGrimlock <gonzalogarat@gmail.com>
2019-07-16 08:58:58 -0300

    16 Jul 2019

--- nigmaJS/README.md
+++ nigmaJS/README.md
@@ -2,6 +2,16 @@
 
 This repository is focused on the implementation of many fantastic cryptographic techniques that have been generated over the last millenia.
 
+Right now:
+
+- Morse: encoding / decoding is ready
+- Baconian: encoding / decoding is ready
+- AMSCO: encoding is ready
+
+If you want to use this modules, know that basicCipher.js is the parent class where all other codes are based. Another intresting fact is that all aux methods are to be placed here. As more methods are implemented and code can be recycled, be sure that basicCipher is only going to get more powerfull.
+
 Feel free to use but know that they are currently being tested/developed for fun purposes only.
 
 I'm not responsible for any trouble caused to you are anyone using this code.
+
+GG
--- nigmaJS/scripts/basicCipher.js
+++ nigmaJS/scripts/basicCipher.js
@@ -1,5 +1,5 @@
 class BasicCipher {
-	/*
+  /*
 	 
 	In cryptography, a cipher (or cypher) is an algorithm for performing encryption or decryption—a series of 
 	well-defined steps that can be followed as a procedure. An alternative, less common term is encipherment. 
@@ -21,60 +21,60 @@ class BasicCipher {
 
 	 */
 
-	constructor(message, encoded, method, key, alphabet) {
-		this.message = message;
-		this.encoded = encoded;
-		this.method = method;
-		this.key = key || "";
-		this.alphabet = alphabet || [];
-	}
-	//GETs
-	getMsg = () => this.message;
+  constructor(message, encoded, method, key, alphabet) {
+    this.message = message;
+    this.encoded = encoded;
+    this.method = method;
+    this.key = key || "";
+    this.alphabet = alphabet || [];
+  }
+  //GETs
+  getMsg = () => this.message;
 
-	getEncoded = () => this.encoded;
+  getEncoded = () => this.encoded;
 
-	getMethod = () => this.method;
+  getMethod = () => this.method;
 
-	getKey = () => this.key;
+  getKey = () => this.key;
 
-	getAlphabet = () => this.alphabet;
+  getAlphabet = () => this.alphabet;
 
-	//SETs
-	setMsg(newMessage) {
-		return (this.message = newMessage);
-	}
+  //SETs
+  setMsg(newMessage) {
+    return (this.message = newMessage);
+  }
 
-	setEncoded(newEncoded) {
-		return (this.encoded = newEncoded);
-	}
+  setEncoded(newEncoded) {
+    return (this.encoded = newEncoded);
+  }
 
-	setMethod(newMethod) {
-		return (this.method = newMethod);
-	}
+  setMethod(newMethod) {
+    return (this.method = newMethod);
+  }
 
-	setKey(newKey) {
-		return (this.key = newKey);
-	}
+  setKey(newKey) {
+    return (this.key = newKey);
+  }
 
-	setAlphabet(newAlphabet) {
-		return (this.alphabet = newAlphabet);
-	}
+  setAlphabet(newAlphabet) {
+    return (this.alphabet = newAlphabet);
+  }
 
-	//Aux methods
+  //Aux methods
 
-	test() {
-		return "NigmaJS enabled";
-	}
+  test() {
+    return "NigmaJS enabled";
+  }
 
-	getKeyByValue = (object, value) =>
-		Object.keys(object).find(key => object[key] === value);
+  getKeyByValue = (object, value) =>
+    Object.keys(object).find(key => object[key] === value);
 
-	sortColumns(a, b) {
-		//Sort values based on the first item on the row
-		if (a[0] === b[0]) {
-			return 0;
-		} else {
-			return a[0] < b[0] ? -1 : 1;
-		}
-	}
+  sortColumns(a, b) {
+    //Sort values based on the first item on the row
+    if (a[0] === b[0]) {
+      return 0;
+    } else {
+      return a[0] < b[0] ? -1 : 1;
+    }
+  }
 }
