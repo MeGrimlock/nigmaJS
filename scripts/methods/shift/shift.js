5992f45fcd9c4e050c1e142ff6b848fca663223c
Me Grimlock <gonzalogarat@gmail.com>
2019-08-05 09:39:34 -0300

    Update shift.js

--- scripts/methods/shift/shift.js
+++ scripts/methods/shift/shift.js
@@ -1,4 +1,8 @@
 import { default as caesarShift } from "./caesarShift.js";
+import { default as rot5 } from "./rot5.js";
+import { default as rot7 } from "./rot7.js";
 import { default as rot13 } from "./rot13.js";
+import { default as rot18 } from "./rot18.js";
+import { default as rot47 } from "./rot47.js";
 
-export default { caesarShift, rot13 };
+export default { caesarShift, rot5, rot7, rot13, rot18, rot47 };
