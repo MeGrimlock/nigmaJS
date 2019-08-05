ded5a19a7b7ec44ad84367ab3f3d8767263826f7
Me Grimlock <gonzalogarat@gmail.com>
2019-08-05 09:34:33 -0300

    Create rot7.js

---
+++ scripts/methods/shift/rot7.js
@@ -0,0 +1,10 @@
+import { default as BasicCipher } from "../../basicCipher.js";
+
+export default class rot7 extends BasicCipher {
+  constructor(message, encoded = false, debug = false) {
+    super(message, encoded, "rot7", 7, "", debug);
+  }
+
+  encode = () => this.shiftCharacters(this.message, this.key);
+  decode = () => this.shiftCharacters(this.message, -this.key);
+}
