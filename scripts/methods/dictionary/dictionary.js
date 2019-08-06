4056939b5115cbba3187d3e55266be951136701e
MeGrimlock <gonzalogarat@gmail.com>
2019-08-06 20:54:11 -0300

    Add autokey to exports list

--- scripts/methods/dictionary/dictionary.js
+++ scripts/methods/dictionary/dictionary.js
@@ -1,6 +1,7 @@
 import { default as atbash } from "./atbash.js";
+import { default as autokey } from "./autoKey.js";
 import { default as baconian } from "./baconian.js";
 import { default as morse } from "./morse.js";
 import { default as simpleSubstitution } from "./simpleSubstitution.js";
 
-export default { atbash, baconian, morse, simpleSubstitution };
+export default { atbash, autokey, baconian, morse, simpleSubstitution };
