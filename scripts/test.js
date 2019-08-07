5887a405d47634b4949fcb46e6eeb203a9e4e697
MeGrimlock <gonzalogarat@gmail.com>
2019-08-06 22:00:21 -0300

    Add .gitignore , simplify test,js

--- scripts/test.js
+++ scripts/test.js
@@ -1,15 +1,15 @@
 //test script for parent and child documents.
 import { default as Nigma, Enigma } from "./nigma.js";
 
-const miTexto4 = new Enigma("Encode this text please");
+const newMachine = new Enigma("Encode this text please");
 
 function output(decrypted, encrypted) {
   console.log("log:", decrypted, encrypted);
   let container = document.getElementById("messages");
   let content = document.createElement("span");
-  encrypted = miTexto4.text2block(encrypted, 5);
+  encrypted = newMachine.text2block(encrypted, 5);
   content.innerHTML = `<h3>Encoding Text: </h3> ${decrypted}<h4> -></h4>${encrypted}<br>`;
   container.appendChild(content);
 }
 
-output(miTexto4.getMsg(), miTexto4.encode());
+output(newMachine.getMsg(), newMachine.encode());
