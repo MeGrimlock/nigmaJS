4e144ccc13817fb5a2e306092f75697b796d44c0
MeGrimlock <gonzalogarat@gmail.com>
2019-08-09 19:20:38 -0300

    Move test.js outside from src and rename to app.js. Update index.html for to work with app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+//test script for parent and child documents.
+import { default as Nigma, Enigma } from "./src/index.js";
+
+const newMachine = new Enigma("Encode this text please");
+
+function output(decrypted, encrypted) {
+  console.log("log:", decrypted, encrypted);
+  let container = document.getElementById("messages");
+  let content = document.createElement("span");
+  encrypted = newMachine.text2block(encrypted, 5);
+  content.innerHTML = `<h3>Encoding Text: </h3> ${decrypted}<h4> -></h4>${encrypted}<br>`;
+  container.appendChild(content);
+}
+
+output(newMachine.getMsg(), newMachine.encode());
