123896c43b9af7e03fae0f5c42069ff7a908b9bb
MeGrimlock <gonzalogarat@gmail.com>
2019-08-13 19:24:01 -0300

    Add files to publish, change main to "app.js"

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require("path");
 
 module.exports = {
-  entry: {
-    app: "./src/test.js"
-  },
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "js/test.bundle.js"
-  },
-  // Loaders
-  module: {
-    rules: [
-      // JavaScript/JSX Files
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      // CSS Files
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  // Plugins
-  plugins: []
+	entry: {
+		app: "./src/index.js"
+	},
+	output: {
+		path: path.resolve(__dirname, "build"),
+		filename: "js/nigma.min.js"
+	},
+	// Loaders
+	module: {
+		rules: [
+			// JavaScript/JSX Files
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"]
+			},
+			// CSS Files
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			}
+		]
+	},
+	// Plugins
+	plugins: []
 };
