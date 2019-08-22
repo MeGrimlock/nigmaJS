cfa85f71c26292246dd8f8cee0a817a9f17d1b37
MeGrimlock <gonzalogarat@gmail.com>
2019-08-22 13:00:24 -0300

    ESLINT formatting

--- app.js
+++ app.js
@@ -7,32 +7,32 @@ import {
 	Shift
 } from "./src/index.js";
 
-function output(decrypted, encrypted) {
+function output(cipher) {
 	//	console.log("log:", decrypted, encrypted);
 	const container = document.getElementById("messages");
 	const content = document.createElement("span");
-	content.innerHTML = `<h3>Encoding Text: </h3> ${decrypted}<h4> -></h4>${encrypted}<br>`;
+	content.innerHTML = `<h3>Encoding Text using: [${cipher.getMethod()}] encryption: </h3> Plaintext: ${cipher.getMsg()} <br> Ciphertext: ${cipher.encode()}<br>`;
 	container.appendChild(content);
 }
 
 const sampleMessage = "Encode this text please";
 // Enigma sample code
 const newMachine = new Enigma(sampleMessage);
-output(newMachine.getMsg(), newMachine.encode());
+output(newMachine);
 
 // morse sample code
 const newMorse = new Dictionary.Morse(sampleMessage);
-output(newMorse.getMsg(), newMorse.encode());
+output(newMorse);
 
 // New Bazeries
 const newBazeries = new Dictionary.Bazeries(
 	"simple substitution plus transposition",
 	"Eighty one thousand two hundred fifty seven"
 );
-output(newBazeries.getMsg(), newBazeries.encode());
+output(newBazeries);
 
 // morse sample code
 const newAtbash = new Dictionary.Atbash(sampleMessage);
-output(newAtbash.getMsg(), newAtbash.encode());
+output(newAtbash);
 newAtbash.setMsg("0r2q10 lxwm l0hl  pt04m0");
 newAtbash.setEncoded(true);
--- src/methods/dictionary/autoKey.js
+++ src/methods/dictionary/autoKey.js
@@ -1,9 +1,9 @@
 import { default as BasicCipher } from "../../basicCipher.js";
 
 export default class AutoKey extends BasicCipher {
-  constructor(message, key, encoded, debug) {
-    super(message, encoded, "autoKey", key, "", debug);
-    /* Parametros: message,encoded,method,key,alphabet
+	constructor(message, key, encoded, debug) {
+		super(message, encoded, "autoKey", key, "", debug);
+		/* Parametros: message,encoded,method,key,alphabet
     This method implements a polialphabet substitution, but instead of generating a Matrix we use a formula that does the same but faster.
     
         A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
@@ -36,59 +36,59 @@ export default class AutoKey extends BasicCipher {
     Z   Z A B C D E F G H I J K L M N O P Q R S T U V W X Y
 
     */
-  }
+	}
 
-  encode = () => {
-    const plaintext = this.message.toLowerCase().replace(/[^a-z]/g, "");
-    const key = this.key.toLowerCase().replace(/[^a-z]/g, "");
-    let ciphertext = "";
-    if (plaintext.length >= 1 && key.length > 1) {
-      for (let i = 0; i < plaintext.length; i++) {
-        if (i < key.length) {
-          ciphertext += String.fromCharCode(
-            ((plaintext.charCodeAt(i) - 97 + (key.charCodeAt(i) - 97) + 26) %
-              26) +
-              97
-          );
-        } else {
-          ciphertext += String.fromCharCode(
-            ((plaintext.charCodeAt(i) -
-              97 +
-              (plaintext.charCodeAt(i - key.length) - 97) +
-              26) %
-              26) +
-              97
-          );
-        }
-      }
-    }
-    return ciphertext;
-  };
+	encode = () => {
+		const plaintext = this.message.toLowerCase().replace(/[^a-z]/g, "");
+		const key = this.key.toLowerCase().replace(/[^a-z]/g, "");
+		let ciphertext = "";
+		if (plaintext.length >= 1 && key.length > 1) {
+			for (let i = 0; i < plaintext.length; i += 1) {
+				if (i < key.length) {
+					ciphertext += String.fromCharCode(
+						((plaintext.charCodeAt(i) - 97 + (key.charCodeAt(i) - 97) + 26) %
+							26) +
+							97
+					);
+				} else {
+					ciphertext += String.fromCharCode(
+						((plaintext.charCodeAt(i) -
+							97 +
+							(plaintext.charCodeAt(i - key.length) - 97) +
+							26) %
+							26) +
+							97
+					);
+				}
+			}
+		}
+		return ciphertext;
+	};
 
-  decode = () => {
-    const ciphertext = this.message.toLowerCase().replace(/[^a-z]/g, "");
-    const key = this.key.toLowerCase().replace(/[^a-z]/g, "");
-    let plaintext = "";
-    if (ciphertext.length > 1 && k.length > 1) {
-      for (i = 0; i < ciphertext.length; i++) {
-        if (i < k.length) {
-          plaintext += String.fromCharCode(
-            ((ciphertext.charCodeAt(i) - 97 - (k.charCodeAt(i) - 97) + 26) %
-              26) +
-              97
-          );
-        } else {
-          plaintext += String.fromCharCode(
-            ((ciphertext.charCodeAt(i) -
-              97 -
-              (plaintext.charCodeAt(i - k.length) - 97) +
-              26) %
-              26) +
-              97
-          );
-        }
-      }
-    }
-    return plaintext;
-  };
+	decode = () => {
+		const ciphertext = this.message.toLowerCase().replace(/[^a-z]/g, "");
+		const key = this.key.toLowerCase().replace(/[^a-z]/g, "");
+		let plaintext = "";
+		if (ciphertext.length > 1 && key.length > 1) {
+			for (let i = 0; i < ciphertext.length; i += 1) {
+				if (i < key.length) {
+					plaintext += String.fromCharCode(
+						((ciphertext.charCodeAt(i) - 97 - (key.charCodeAt(i) - 97) + 26) %
+							26) +
+							97
+					);
+				} else {
+					plaintext += String.fromCharCode(
+						((ciphertext.charCodeAt(i) -
+							97 -
+							(plaintext.charCodeAt(i - key.length) - 97) +
+							26) %
+							26) +
+							97
+					);
+				}
+			}
+		}
+		return plaintext;
+	};
 }
