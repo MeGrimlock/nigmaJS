dfe0f3c094de0124416003fe86703125a1a72a02
MeGrimlock <gonzalogarat@gmail.com>
2019-09-03 22:15:32 -0300

    Update entry to app.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	entry: {
 		// app: "./src/index.js"
-		app: './src/index.js'
+		app: './app.js'
 	},
 	output: {
 		path: path.resolve(__dirname, 'build'),
