e3f3be58103f8f011e4828f92fced2cb9bff7f20
MeGrimlock <gonzalogarat@gmail.com>
2019-09-07 17:15:55 -0300

    JSDoc update + allow to encode/decode more dinamically.

--- src/methods/shift/caesarShift.js
+++ src/methods/shift/caesarShift.js
@@ -1,9 +1,7 @@
-import { default as BasicCipher } from '../../basicCipher.js';
+import { default as BasicCipher } from "../../basicCipher.js";
 
-export default class CaesarShift extends BasicCipher {
-	/*
-   
-   Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, 
+/**
+ * Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, 
    is one of the simplest and most widely known encryption techniques. 
    
    It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter 
@@ -20,22 +18,32 @@ export default class CaesarShift extends BasicCipher {
    https://en.wikipedia.org/wiki/Caesar_cipher
    
    Note: Since this is a really basic encryption method, it is included in basicCipher class
+  @param {String} message this text is what we want to get encoded/decoded
+  @param {number} key a number that is used to shift the alphabet
+  @param {Boolean} encoded optional value that indicates if the message is already encoded
+  @param {Boolean} debug optional value used to show/ debug messages
+ */
+
+export default class CaesarShift extends BasicCipher {
+  constructor(message, key, encoded = false, debug = false) {
+    super(message, encoded, "caesarShift", key, "", debug);
+  }
+
+  /**
+   * @method encode
+   * @param {String} message text to use for encoding, if empty use stored message
+   * @param {number} key key to use for encoding, if empty use stored key
    */
 
-	constructor(message, key, encoded = false, debug = false) {
-		/* console.log(
-      `AMSCO Constructor> KEY :${key} Encoded:${encoded} DEBUG:${debug}\n Msg: ${message} \n `
-    ); */
-		super(message, encoded, 'caesarShift', key, '', debug);
+  encode = (message = this.message, myKey = this.key) =>
+    this.shiftCharacters(message, myKey);
 
-		// Parametros: message,encoded,method,key,alphabet
-		// this.decode.bind(this);
-		// logMessage("constuctor",this);
-	}
+  /**
+   * @method decode
+   * @param {String} message text to use for decoding, if empty use stored message
+   * @param {number} key input the same key used for encoding, if empty method uses stored key
+   */
 
-	encode = () => this.shiftCharacters(this.message, this.key);
-
-	decode = () => this.shiftCharacters(this.message, -this.key);
+  decode = (message = this.message, myKey = this.key) =>
+    this.shiftCharacters(message, -myKey);
 }
-
-// export default caesarShift;
