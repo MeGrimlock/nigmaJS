72bc1060654aea9878ef836974f1bd02523727b2
MeGrimlock <gonzalogarat@gmail.com>
2019-09-07 16:03:37 -0300

    JSDoc comments update

--- src/methods/dictionary/autoKey.js
+++ src/methods/dictionary/autoKey.js
@@ -1,20 +1,20 @@
-import { default as BasicCipher } from '../../basicCipher.js';
+import { default as BasicCipher } from "../../basicCipher.js";
 
 /**
  * This method implements a polialphabet substitution, but instead of generating a Matrix we use a formula that does the same but faster.
- * An autokey cipher (also known as the autoclave cipher) is a cipher that incorporates the message (the plaintext) into the key. 
- * The key is generated from the message in some automated fashion, sometimes by selecting certain letters from the text or, more commonly, 
+ * An autokey cipher (also known as the autoclave cipher) is a cipher that incorporates the message (the plaintext) into the key.
+ * The key is generated from the message in some automated fashion, sometimes by selecting certain letters from the text or, more commonly,
  * by adding a short primer key to the front of the message.
- * 
+ *
  * @method constructor
- * @param {String} message 
+ * @param {String} message this text is what we want to get encoded/decoded
  * @param {String} key a text that is used to generate the alphabet
  */
 
 export default class AutoKey extends BasicCipher {
-	constructor(message, key, encoded, debug) {
-		super(message, encoded, 'autoKey', key, '', debug);
-		    
+  constructor(message, key, encoded, debug) {
+    super(message, encoded, "autoKey", key, "", debug);
+
     /*
         A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
         ---------------------------------------------------
@@ -46,71 +46,71 @@ export default class AutoKey extends BasicCipher {
     Z   Z A B C D E F G H I J K L M N O P Q R S T U V W X Y
 
     */
-	}
+  }
 
-	/**
-	 * @method encode
-	 * @param {String} message text to use for encoding, if empty use stored message
-	 * @param {String} key key to use for encoding, if empty use stored key
-	 */
+  /**
+   * @method encode
+   * @param {String} message text to use for encoding, if empty use stored message
+   * @param {String} key key to use for encoding, if empty use stored key
+   */
 
-	encode = (message=this.message,myKey=this.key) => {
-		const plaintext = message.toLowerCase().replace(/[^a-z]/g, '');
-		const key = myKey.toLowerCase().replace(/[^a-z]/g, '');
-		let ciphertext = '';
-		if (plaintext.length >= 1 && key.length > 1) {
-			for (let i = 0; i < plaintext.length; i += 1) {
-				if (i < key.length) {
-					ciphertext += String.fromCharCode(
-						((plaintext.charCodeAt(i) - 97 + (key.charCodeAt(i) - 97) + 26) %
-							26) +
-							97
-					);
-				} else {
-					ciphertext += String.fromCharCode(
-						((plaintext.charCodeAt(i) -
-							97 +
-							(plaintext.charCodeAt(i - key.length) - 97) +
-							26) %
-							26) +
-							97
-					);
-				}
-			}
-		}
-		return ciphertext;
-	};
+  encode = (message = this.message, myKey = this.key) => {
+    const plaintext = message.toLowerCase().replace(/[^a-z]/g, "");
+    const key = myKey.toLowerCase().replace(/[^a-z]/g, "");
+    let ciphertext = "";
+    if (plaintext.length >= 1 && key.length > 1) {
+      for (let i = 0; i < plaintext.length; i += 1) {
+        if (i < key.length) {
+          ciphertext += String.fromCharCode(
+            ((plaintext.charCodeAt(i) - 97 + (key.charCodeAt(i) - 97) + 26) %
+              26) +
+              97
+          );
+        } else {
+          ciphertext += String.fromCharCode(
+            ((plaintext.charCodeAt(i) -
+              97 +
+              (plaintext.charCodeAt(i - key.length) - 97) +
+              26) %
+              26) +
+              97
+          );
+        }
+      }
+    }
+    return ciphertext;
+  };
 
-	/**
-	 * @method decode
-	 * @param {String} message text to use for decoding, if empty use stored message
-	 * @param {String} key key to use for decoding, if empty use stored key
-	 */
+  /**
+   * @method decode
+   * @param {String} message text to use for decoding, if empty use stored message
+   * @param {String} key key to use for decoding, if empty use stored key
+   */
 
-	decode = (message=this.message,myKey=this.key) => {
-		const ciphertext = message.toLowerCase().replace(/[^a-z]/g, '');
-		const key = myKey.toLowerCase().replace(/[^a-z]/g, '');
-		let plaintext = '';
-		if (ciphertext.length > 1 && key.length > 1) {
-			for (let i = 0; i < ciphertext.length; i += 1) {
-				if (i < key.length) {
-					plaintext += String.fromCharCode(
-						((ciphertext.charCodeAt(i) - 97 - (key.charCodeAt(i) - 97) + 26) %
-							26) +
-							97
-					);
-				} else {
-					plaintext += String.fromCharCode(
-						((ciphertext.charCodeAt(i) -
-							97 -
-							(plaintext.charCodeAt(i - key.length) - 97) +
-							26) %
-							26) +
-							97
-					);
-				}
-			}
-		}
-		return plaintext;
-	};
+  decode = (message = this.message, myKey = this.key) => {
+    const ciphertext = message.toLowerCase().replace(/[^a-z]/g, "");
+    const key = myKey.toLowerCase().replace(/[^a-z]/g, "");
+    let plaintext = "";
+    if (ciphertext.length > 1 && key.length > 1) {
+      for (let i = 0; i < ciphertext.length; i += 1) {
+        if (i < key.length) {
+          plaintext += String.fromCharCode(
+            ((ciphertext.charCodeAt(i) - 97 - (key.charCodeAt(i) - 97) + 26) %
+              26) +
+              97
+          );
+        } else {
+          plaintext += String.fromCharCode(
+            ((ciphertext.charCodeAt(i) -
+              97 -
+              (plaintext.charCodeAt(i - key.length) - 97) +
+              26) %
+              26) +
+              97
+          );
+        }
+      }
+    }
+    return plaintext;
+  };
 }
