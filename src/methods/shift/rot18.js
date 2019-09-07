967276bff83a3e4d7e77d23c39a8d9ae6bce72b2
MeGrimlock <gonzalogarat@gmail.com>
2019-09-07 17:23:49 -0300

    JDodc update + pass msg and key to encode/decode

--- src/methods/shift/rot18.js
+++ src/methods/shift/rot18.js
@@ -1,13 +1,27 @@
-import { default as BasicCipher } from '../../basicCipher.js';
+import { default as BasicCipher } from "../../basicCipher.js";
 
 export default class Rot18 extends BasicCipher {
-	constructor(message, encoded = false, debug = false) {
-		super(message, encoded, 'rot18', 18, '', debug);
-	}
+  constructor(message, encoded = false, debug = false) {
+    super(message, encoded, "rot18", 18, "", debug);
+  }
 
-	encode = () => this.shiftCharacters(this.message, this.key);
+  /**
+   * @method encode
+   * @param {String} message text to use for encoding, if empty use stored message
+   * @param {number} key key to use for encoding, if empty use stored key
+   */
 
-	decode = () => this.shiftCharacters(this.message, -this.key);
+  encode = (message = this.message, myKey = this.key) =>
+    this.shiftCharacters(message, myKey);
+
+  /**
+   * @method decode
+   * @param {String} message text to use for decoding, if empty use stored message
+   * @param {number} key input the same key used for encoding, if empty method uses stored key
+   */
+
+  decode = (message = this.message, myKey = this.key) =>
+    this.shiftCharacters(message, -myKey);
 }
 
 // export default rot18;
